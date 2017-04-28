32d62cd50d9173e4d3f01666a4f1f0cc21ff06e1
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-04-28 16:05:33 -0700

    Something operational

--- 14-middleware/code-demo/public/routes.js
+++ 14-middleware/code-demo/public/routes.js
@@ -1,5 +1,22 @@
-page('/success', success);
-page('/*', index);
+'use strict';
+
+page('/', index, success);
 page();
 
-// in some other file, like app.js
+function index (ctx, next) {
+  $('#the-form').on('submit', function(event){
+    event.preventDefault();
+    let $username = $('#username').val();
+    if ($username === 'someUser') {
+      ctx.state.user = $username;
+      ctx.save();
+      next();
+    }
+  });
+};
+
+function success (ctx) {
+  if (ctx.state.user) {
+    $('body').html(`<h1>You are logged in for the success function! </h1>`);
+  }
+}
